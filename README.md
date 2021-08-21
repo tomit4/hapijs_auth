89deca29fd590582b637ab70e64544cd6ff8ae23
tomit4 <mosssap@gmail.com>
2021-08-21 10:23:57 -0700

    basic hapijs authentication

---
+++ README.md
@@ -0,0 +1,7 @@
+<h2>The Beginner's Basics to User Authentication in HapiJS</h2>
+
+This is a very quick tutorial following of Web Dev Simplified's Youtube tutorial on basic authentication utilizing HapiJS and Bcrypt. It very quickly sets up a HapiJS server and exemplifies the basics of user creation with a Bcrypt Hashed/Salted Password and then exemplifies the essentials of Logging in.
+
+The link for the tutorial can be found <a style=color:#00ae8cff href="https://www.youtube.com/watch?v=Ud5xKCYQTjM">here.</a>
+
+_This version was adapted to be used with HapiJS. This is meant to eventually translate to a basic authentication page using BCrypt and HapiJS, I have yet to explore the possibilities with Joi, but it is something that I will look into once the basics are set up._
---
+++ app.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const Hapi = require("@hapi/hapi")
+const bcrypt = require('bcrypt')
+
+const init = async() => {
+
+    const users = []
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/users',
+        handler: async(req, h) => {
+            return users
+        }
+    })
+
+    server.route({
+        method: 'POST',
+        path: '/users',
+        handler: async(req, h) => {
+                const hashedPassword = await bcrypt.hash(req.payload.password, 10)
+                const user = {
+                    name: req.payload.name,
+                    password: hashedPassword
+                }
+                users.push(user)
+                return "Created User Profile"
+            
+        }         
+    })
+
+    server.route({
+        method: 'POST',
+        path: '/user/login',
+        handler: async(req, h) => {
+            const user = users.find(user => user.name = req.payload.name)
+
+            if (user == null) {
+                return "Cannot Find User"
+            }
+            try {
+                if (await bcrypt.compare(req.payload.password, user.password)) {
+                    return "Success"
+                } else {
+                    return "Not Allowed"
+                }
+            } catch {
+                return "Status 500 Error"
+            }
+        }
+    })
+
+    await server.start()
+    console.log('Server running on %s', server.info.uri)
+
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err)
+    process.exit(1)
+})
+
+init()
