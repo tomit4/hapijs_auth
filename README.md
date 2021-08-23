c1f73a9808f73635070b935cfedfeaaab91df062
tomit4 <mosssap@gmail.com>
2021-08-23 03:42:17 -0700

    created front end to create users

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 <h2>The Beginner's Basics to User Authentication in HapiJS</h2>
 
-This is a very quick tutorial following of Web Dev Simplified's Youtube tutorial on basic authentication utilizing HapiJS and Bcrypt. It very quickly sets up a HapiJS server and exemplifies the basics of user creation with a Bcrypt Hashed/Salted Password and then exemplifies the essentials of Logging in.
+I have begun to modify this original adaptation of Web Dev Simplified's Tutorial on ExpressJS Authentication which I simply adjusted to use HapiJS.  After going through WittCode's Youtube Tutorial Series on HapiJS, I have made some very small adjustments to create a Create User Screen.  This will eventually be  modified to be used as a default server side rendered HTML page that can create a user and log them into a hidden html page.  There are probably more plugins I will need, but as it is it uses only a handful of Node Modules and one Hapi Plugin (@hapi/insert) at this time.
 
-The link for the tutorial can be found <a style=color:#00ae8cff href="https://www.youtube.com/watch?v=Ud5xKCYQTjM">here.</a>
-
-_This version was adapted to be used with HapiJS (webdevsimplified's version uses ExpressJS). This is meant to eventually translate to a basic authentication page using BCrypt and HapiJS, I have yet to explore the possibilities with Joi, but it is something that I will look into once the basics are set up._
+_This repository is a work in progress to help assist me in understanding the basics of authentication._
--- app.js
+++ app.js
@@ -1,83 +1,127 @@
-'use strict'
+"use strict";
+const path = require("path");
+const Hapi = require("@hapi/hapi");
+const Inert = require("@hapi/inert");
+const bcrypt = require("bcrypt");
+const pool = require("./maria_database_connection/database.js");
 
-const Hapi = require("@hapi/hapi")
-const bcrypt = require('bcrypt')
-const pool = require('./maria_database_connection/database.js')
+const init = async () => {
 
-const init = async() => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+    routes: {
+        cors: true,
+        files: {
+            relativeTo: path.join(__dirname, "static"),
+        },
+    },
+  });
 
-    const users = []
+// Hapi Plugin Registrations
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    })
+  await server.register([
+    {
+      plugin: Inert,
+    },
+  ]);
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: async(req, h) => {
-            return users
-        }
-    })
-
-    server.route({
-        method: 'GET',
-        path: '/users',
-        handler: async() => {
-            const sqlQuery = 'SELECT * FROM users;'
-            const result = await pool.query(sqlQuery)
-            return result
-        }               
-    })
-
-    server.route({
-        method: 'POST',
-        path: '/users',
-        handler: async(req, h) => {
-                const hashedPassword = await bcrypt.hash(req.payload.password, 10)
+  server.route(
+    [
+        {
+          method: "GET",
+            path: "/",
+            handler: async (req, h) => {
+            return h.file("./create_user.html");
+            }
+        },
+        {
+            method: "POST",
+            path: "/",
+            handler: async (req, h) => {
+            if(req.payload.username !== "" && req.payload.password !== "") {
+                const hashedPassword = await bcrypt.hash(req.payload.password, 10);
                 const user = {
-                    name: req.payload.name,
-                    password: hashedPassword
-                }
-                // users.push(user)
-                // return "Created User Profile " + hashedPassword
-                const sqlQuery = 'INSERT INTO users (username, password) VALUES (?, ?)'
-                const result = await pool.query(sqlQuery, [user.name, hashedPassword])
-                return result
-            
-        }         
-    })
-
-    server.route({
-        method: 'POST',
-        path: '/user/login',
-        handler: async(req, h) => {
-            const user = users.find(user => user.name = req.payload.name)
-
+                    username: req.payload.username,
+                    password: hashedPassword,
+                };
+                const sqlQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
+                const result = await pool.query(sqlQuery, [user.username, hashedPassword]);
+                return h.redirect("/success")
+            } else {
+                return h.redirect("/")
+            }
+            }
+        },
+        {
+            method: 'GET',
+            path: '/success',
+            handler: (request, h) => {
+                return h.file("./success_create.html")
+            }
+        },
+        {
+            method: 'GET',
+            path: '/{any*}',
+            handler: (request, h) => {
+                return h.file("./404.html")
+            }
+        },
+        {
+            method: "GET",
+            path: "/users",
+            handler: async () => {
+            const sqlQuery = "SELECT * FROM users;";
+            const result = await pool.query(sqlQuery);
+            return result;
+            }
+        },
+        {
+            method: "POST",
+            path: "/users",
+            handler: async (req, h) => {
+            const hashedPassword = await bcrypt.hash(req.payload.password, 10);
+            const user = {
+                name: req.payload.name,
+                password: hashedPassword,
+            };
+            const sqlQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
+            const result = await pool.query(sqlQuery, [user.name, hashedPassword]);
+            return result;
+            }
+        },
+        {
+            method: "POST",
+            path: "/user/login",
+            handler: async (req, h) => {
+            const user = users.find((user) => (user.name = req.payload.name));
+        
             if (user == null) {
-                return "Cannot Find User"
+                return "Cannot Find User";
             }
             try {
                 if (await bcrypt.compare(req.payload.password, user.password)) {
-                    return "Success"
+                return "Success";
                 } else {
-                    return "Not Allowed"
+                return "Not Allowed";
                 }
             } catch {
-                return "Status 500 Error"
+                return "Status 500 Error";
+            }
             }
         }
-    })
+    ])
 
-    await server.start()
-    console.log('Server running on %s', server.info.uri)
-
+await server.start();
+console.log("Server running on %s", server.info.uri);
 }
 
-process.on('unhandledRejection', (err) => {
-    console.log(err)
-    process.exit(1)
-})
 
-init()
+
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
