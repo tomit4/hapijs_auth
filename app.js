9ca6e8d2462c8775d5f1f47f4b1bdc52794b7cf5
tomit4 <mosssap@gmail.com>
2021-08-24 13:20:18 -0700

    starting to code password comparison

--- app.js
+++ app.js
@@ -7,121 +7,146 @@ const pool = require("./maria_database_connection/database.js");
 
 const init = async () => {
 
-  const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
-    routes: {
-        cors: true,
-        files: {
-            relativeTo: path.join(__dirname, "static"),
+    const server = Hapi.server({
+        port: 3000,
+        host: "localhost",
+        routes: {
+            cors: true,
+            files: {
+                relativeTo: path.join(__dirname, "static"),
+            },
         },
-    },
-  });
+    });
 
-// Hapi Plugin Registrations
+    // Hapi Plugin Registrations
 
-  await server.register([
-    {
-      plugin: Inert,
-    },
-  ]);
+    await server.register([{
+        plugin: Inert,
+    }, ]);
 
-  server.route(
-    [
-        {
-          method: "GET",
-            path: "/",
-            handler: async (req, h) => {
-            return h.file("./create_user.html");
-            }
-        },
-        {
-            method: "POST",
-            path: "/",
-            handler: async (req, h) => {
-            if(req.payload.username !== "" && req.payload.password !== "") {
-                const hashedPassword = await bcrypt.hash(req.payload.password, 10);
-                const user = {
-                    username: req.payload.username,
-                    password: hashedPassword,
-                };
-                const sqlQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
-                const result = await pool.query(sqlQuery, [user.username, hashedPassword]);
-                return h.redirect("/success")
-            } else {
-                return h.redirect("/")
-            }
-            }
-        },
-        {
-            method: 'GET',
-            path: '/success',
-            handler: (request, h) => {
-                return h.file("./success_create.html")
-            }
-        },
-        {
-            method: 'GET',
-            path: '/{any*}',
-            handler: (request, h) => {
-                return h.file("./404.html")
-            }
-        },
-        {
-            method: "GET",
-            path: "/users",
-            handler: async () => {
-            const sqlQuery = "SELECT * FROM users;";
-            const result = await pool.query(sqlQuery);
-            return result;
-            }
-        },
-        {
-            method: "POST",
-            path: "/users",
-            handler: async (req, h) => {
-            const hashedPassword = await bcrypt.hash(req.payload.password, 10);
-            const user = {
-                name: req.payload.name,
-                password: hashedPassword,
-            };
-            const sqlQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
-            const result = await pool.query(sqlQuery, [user.name, hashedPassword]);
-            return result;
-            }
-        },
-        {
-            method: "POST",
-            path: "/user/login",
-            handler: async (req, h) => {
-            const user = users.find((user) => (user.name = req.payload.name));
-        
-            if (user == null) {
-                return "Cannot Find User";
-            }
-            try {
-                if (await bcrypt.compare(req.payload.password, user.password)) {
-                return "Success";
-                } else {
-                return "Not Allowed";
+    server.route(
+        [{
+                method: "GET",
+                path: "/",
+                handler: async (req, h) => {
+                    return h.file("./login.html");
                 }
-            } catch {
-                return "Status 500 Error";
-            }
-            }
-        }
-    ])
+            },
+            {
+                method: "GET",
+                path: "/create_user",
+                handler: async (req, h) => {
+                    return h.file("./create_user.html")
+                }
+            },
+            {
+                method: "POST",
+                path: "/create_user",
+                handler: async (req, h) => {
+                    if (req.payload.username !== "" && req.payload.password !== "") {
+                        const hashedPassword = await bcrypt.hash(req.payload.password, 10);
+                        const user = {
+                            username: req.payload.username,
+                            password: hashedPassword,
+                        };
+                        const sqlQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
+                        const result = await pool.query(sqlQuery, [user.username, hashedPassword]);
+                        return h.redirect("/success")
+                    } else {
+                        return h.redirect("/")
+                    }
+                }
+            },
+            {
+                method: 'GET',
+                path: '/success',
+                handler: (request, h) => {
+                    return h.file("./success_create.html")
+                }
+            },
+            {
+                method: 'GET',
+                path: '/{any*}',
+                handler: (request, h) => {
+                    return h.file("./404.html")
+                }
+            },
+            {
+                method: "GET",
+                path: "/users",
+                handler: async () => {
+                    const sqlQuery = "SELECT * FROM users;";
+                    const result = await pool.query(sqlQuery);
+                    return result;
+                }
+            },
+            {
+                method: "POST",
+                path: "/users",
+                handler: async (req, h) => {
+                    const hashedPassword = await bcrypt.hash(req.payload.password, 10);
+                    const user = {
+                        name: req.payload.username,
+                        password: hashedPassword,
+                    };
+                    const sqlQuery = "INSERT INTO users (username, password) VALUES (?, ?);";
+                    const result = await pool.query(sqlQuery, [user.name, hashedPassword]);
+                    return result;
+                }
+            },
+            {
+                method: "POST",
+                path: "/",
+                handler: async (req, h) => {
 
-await server.start();
-console.log("Server running on %s", server.info.uri);
+                    // we should reference req.payload.username req.payload.password (input)
+
+                    const sqlQuery = "SELECT * FROM users";
+                    const result = await pool.query(sqlQuery);
+
+                    for (let i = 0; i < result.length; i++) { // works, finds the user based off of username... now we have to compare bcrypt passwords...
+                        if (req.payload.username === result[i].username) {
+                            return result[i]
+                        } else {
+                            return "No Dice"
+                        }
+                    }
+
+                    // if (await bcrypt.compare(user.password, req.payload.password)) {
+                    //     return "Success"
+                    // } else {
+                    //     return "Not Allowed"
+                    // }
+
+
+                    //     const user = users.find((user) => (user.name = req.payload.name));
+
+                    //     if (user == null) {
+                    //         return "Cannot Find User";
+                    //     }
+                    //     try {
+                    //         if (await bcrypt.compare(req.payload.password, user.password)) {
+                    //             return "Success";
+                    //         } else {
+                    //             return "Not Allowed";
+                    //         }
+                    //     } catch {
+                    //         return "Status 500 Error";
+                    //     }
+                }
+            }
+        ])
+
+    await server.start();
+    console.log("Server running on %s", server.info.uri);
 }
 
 
 
 
 process.on("unhandledRejection", (err) => {
-  console.log(err);
-  process.exit(1);
+    console.log(err);
+    process.exit(1);
 });
 
 init();
